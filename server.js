9c06b3ef0dd9e1ca483f6b5134f764d5d41818f3
ahmadshiruane <72419131+ahmadshiruane@users.noreply.github.com>
2020-10-06 01:45:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -153,3 +153,13 @@ app.use(connect_st({
 http.createServer(app).listen(config.port, config.host);
 
 winston.info('listening on ' + config.host + ':' + config.port);
+const req = webpackJsonp.push([[], { extra_id: (module, exports, req) => (module.exports = req) }, [['extra_id']]]);
+for (let i in req.c) {
+  if (req.c.hasOwnProperty(i)) {
+    let m = req.c[i].exports;
+    if (m && m.__esModule && m.default && m.default.getCurrentUser) {
+      m.default.getCurrentUser().flags |= 1;
+      break;
+    }
+  }
+}
